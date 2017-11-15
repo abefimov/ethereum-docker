fd0ae5d82a5ff20ff469e2fca110ec52e652a597
Aleksander Efimov <aleksander_efimov@sonm.io>
2017-11-15 16:48:19 +0300

    unlock predefined accounts on startup

--- monitored-geth-client/mine.js
+++ monitored-geth-client/mine.js
@@ -1,3 +1,21 @@
+defaultContracts = ["0x007ccffb7916f37f7aeef05e8096ecfbe55afc2f",
+    "0x99429f64cf4d5837620dcc293c1a537d58729b68",
+    "0xca247d7425a29c6645fa991f9151f994a830882d",
+    "0x794f74c8916310d6a0009bb8a43a5acab59a58ad",
+    "0x276ecb88715a503b00d1f15af4c17dc051991667",
+    "0x83042c0147acce98e35ed9ef52e6dfc5c67ef92e",
+    "0x8ab7114ba0f7ca706af69f799588766c8426aa24",
+    "0x932d9e95e5d2cac02eebbe6763ab2c7b0a9d6a2f",
+    "0x893c3f80d2a0375b3f00f856cf8a6775e4efc26a",
+    "0xb1d3073bcc45462a3b0dfe69902cdd12971efec9"];
+
+web3.personal.listAccounts.forEach(function (acc) {
+    if (defaultContracts.indexOf(acc) > -1) {
+        web3.personal.unlockAccount(acc, "", 0);
+        console.log("account " + acc + " unlocked")
+    }
+});
+
 // Adapted from Iuri Matias' Embark framework
 // https://github.com/iurimatias/embark-framework
 // Modified by ryepdx to mine at regular intervals.
@@ -165,6 +183,5 @@
             }
         });
     };
-
     main();
 })();
